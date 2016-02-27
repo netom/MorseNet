86dc955623e58eac9cea4a2b6e1095d8b0bcc190
Fábián Tamás László <giganetom@gmail.com>
2016-02-27 22:01:00 +0100

    Use shared

--- cwdecode/rnn.py
+++ cwdecode/rnn.py
@@ -14,33 +14,31 @@ N_CLASSES  = len(MORSE_CHR)
 
 # Architecture:
 #
-# 1st layer: sigmoid, (CHUNK,CHUNK)
-# 2nd layer: sigmoid, output fed back with new input (CHUNK+N_CLASSES,N_CLASSES)
+# 1st layer: softplus, (CHUNK,CHUNK)
+# 2nd layer: softplus, output fed back with new input (CHUNK+N_CLASSES,N_CLASSES)
 # 3rd layer: softmax classifier (N_CLASSES, N_CLASSES)
 class RNN:
-    x   = T.fvector('x') # Input vector
-    w1  = T.fmatrix('w1')
-    b1  = T.fvector('b1')
-    w2  = T.fmatrix('w2')
-    b2  = T.fvector('b2')
-    o2_ = T.fvector('o2')
-    o2  = T.fvector('o2')
-    w3  = T.fmatrix('w3')
-    b3  = T.fvector('b3')
-
-    o1 = T.nnet.softplus(T.dot(x, w1) + b1)
-    o2 = T.nnet.softplus(T.dot(T.concatenate([o1, o2_]), w2) + b2)
-    o3 = T.nnet.softmax(T.dot(o2, w3) + b3)
-
-    f = theano.function(inputs=[x, w1, b1, w2, b2, o2_, w3, b3], outputs=[o3, o2])
-
     def __init__(self):
+        x   = T.fvector('x') # Input vector
+        w1  = T.fmatrix('w1')
+        b1  = T.fvector('b1')
+        w2  = T.fmatrix('w2')
+        b2  = T.fvector('b2')
+        o2_ = theano.shared(np.zeros(N_CLASSES, dtype=np.float32), 'o2')
+        w3  = T.fmatrix('w3')
+        b3  = T.fvector('b3')
+
+        o1 = T.nnet.softplus(T.dot(x, w1) + b1)
+        o2 = T.nnet.softplus(T.dot(T.concatenate([o1, o2_]), w2) + b2)
+        o3 = T.nnet.softmax(T.dot(o2, w3) + b3)
+
+        self.f = theano.function(inputs=[x, w1, b1, w2, b2, w3, b3], outputs=o3, updates=[(o2_, o2)])
+
         self.params = [
             np.random.randn(CHUNK, CHUNK).astype(np.float32) * 0.01,               # w1
             np.zeros(CHUNK, dtype=np.float32),                                     # b1
             np.random.randn(CHUNK+N_CLASSES, N_CLASSES).astype(np.float32) * 0.01, # w2
             np.zeros(N_CLASSES, dtype=np.float32),                                 # b2
-            np.zeros(N_CLASSES, dtype=np.float32),                                 # o2
             np.random.randn(N_CLASSES, N_CLASSES).astype(np.float32) * 0.01,       # w3
             np.zeros(N_CLASSES, dtype=np.float32)                                  # b3
         ]
@@ -49,19 +47,12 @@ class RNN:
         return self.f(*([x] + self.params))
 
     def neighbour(self, learning_rate):
-        nn = Net()
-        nn.params = [
-            self.params[0] + np.random.randn(CHUNK, CHUNK) * learning_rate,               # w1
-            self.params[1] + np.random.randn(CHUNK) * learning_rate,                      # b1
-            self.params[2] + np.random.randn(CHUNK+N_CLASSES, N_CLASSES) * learning_rate, # w2
-            self.params[3] + np.random.randn(N_CLASSES) * learning_rate,                  # b2
-            self.params[4],                                                               # o2
-            self.params[5] + np.random.randn(N_CLASSES, N_CLASSES) * learning_rate,       # w3
-            self.params[6] + np.random.randn(N_CLASSES) * learning_rate                   # b3
-        ]
-        return nn
+        params = []
+        for i in xrange(len(self.params)):
+            params.append(self.params[i] + (np.random.randn(*self.params[i].shape) - 0.5) * learning_rate)
+        return params
 
-    def mse(self, examples, targets):
+    def mse(self, example, target):
         return self.f_mse(*([examples, targets] + self.params))
 
 rnn = RNN()
@@ -71,8 +62,8 @@ with open('training_set/sample_0.pickle', 'r') as f:
 
 print "Calculating..."
 for chunk in chunks:
-    y, o2 = rnn.propagate(chunk)
-    rnn.params[4] = o2
+    y = rnn.propagate(chunk)
+    print y
 
 print "Done"
 
